ea66eccb18320c0e0ff5e32ab52eea3c35a4a51c
DBNubs <iam@jasonblanda.com>
2021-03-11 17:01:16 -0500

    Session storage for color modes.

--- js/dark-mode-toggle.js
+++ js/dark-mode-toggle.js
@@ -1,29 +1,40 @@
 (function ($, Drupal) {
   Drupal.behaviors.toggle_dark_mode = {
     'attach': function(contenxt, settings) {
+      // set storage
+      if (window.matchMedia &&
+          window.matchMedia('(prefers-color-scheme: dark)').matches
+          && sessionStorage.getItem('colormode') != 'light-mode') {
+        sessionStorage.setItem('colormode', 'dark-mode');
+      } else if (window.matchMedia &&
+          window.matchMedia('(prefers-color-scheme: light)').matches
+          && sessionStorage.getItem('colormode') != 'dark-mode') {
+        sessionStorage.setItem('colormode', 'light-mode');
+      }
+
       // Trigger to control modes
-      var $darkmode = false;
-      if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      if (sessionStorage.getItem('colormode') != 'dark-mode') {
+        $('body').addClass('light-mode');
+        $('.toggle-dark-mode').text('Dark Mode');
+        sessionStorage.setItem('colormode', 'light-mode');
+      }
+      if (sessionStorage.getItem('colormode') != 'light-mode') {
         $('body').addClass('dark-mode');
         $('.toggle-dark-mode').text('Light Mode');
-        $darkmode = true;
-      } else {
-        $('body').once().addClass('light-mode');
-        $('.toggle-dark-mode').text('Dark Mode');
-        $darkmode = false;
+        sessionStorage.setItem('colormode', 'dark-mode');
       }
 
       $('.toggle-dark-mode').click(function() {
-        if($darkmode == false) {
+        if(sessionStorage.getItem('colormode') == 'dark-mode') {
+          $(this).text('Dark Mode');
+          $('body').addClass('light-mode');
+          $('body').removeClass('dark-mode');
+          sessionStorage.setItem('colormode', 'light-mode');
+        } else {
           $(this).text('Light Mode');
           $('body').addClass('dark-mode');
           $('body').removeClass('light-mode');
-          $darkmode = true;
-        } else {
-          $(this).text('Dark Mode');
-          $('body').removeClass('dark-mode');
-          $('body').addClass('light-mode');
-          $darkmode = false;
+          sessionStorage.setItem('colormode', 'dark-mode');
         }
       });
     }
